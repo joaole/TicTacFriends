6debf15bb550b911f404019486d9f99e427c952f
joaole <joaole64@gmail.com>
2024-10-12 11:56:19 -0300

    new file:   .gitignore 	new file:   .prettierrc 	new file:   jsconfig.json 	new file:   package-lock.json 	new file:   package.json 	new file:   public/index.html 	new file:   src/App.js 	new file:   src/Game.js 	new file:   src/index.js 	new file:   src/styles.css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+import Game from './Game.js';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>TicTacFriends O Clássico da Ilha</h1>
+      <Game />
+    </div>
+  );
+}
