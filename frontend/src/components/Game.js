d51968cb1f57872a0cfae5d11aad4793cc95dfb2
joaole <joaole64@gmail.com>
2024-12-09 11:15:09 -0300

    modified:   frontend/src/components/Game.js

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+const socket = io('http://localhost:5000');
 
 const Game = ({ username, opponent }) => {
   const [board, setBoard] = useState(Array(9).fill(null));
